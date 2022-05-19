8e6dc069910a93cfb82589e71cf9a933faf58ecf
Sarah Thompson <sthompson@fintechprocessing.com>
2022-05-19 14:48:46 -0400

    added data into tables via seeds file

---
+++ db/seeds.sql
@@ -0,0 +1,16 @@
+INSERT INTO department (name)
+VALUES 
+('sales'),
+('engineering'),
+('finance'),
+('legal');
+
+INSERT INTO roles (title, salary)
+VALUES
+('salesperson','8000'),
+('lead engineer','150000'),
+('software engineer','120000'),
+('account manager','160000'),
+('accountant','125000'),
+('legal team lead', '250000'),
+('lawyer','190000');
