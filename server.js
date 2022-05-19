37b24c4c1c0a4286a221ceeea51c643e41349d70
Sarah Thompson <sthompson@fintechprocessing.com>
2022-05-19 17:57:03 -0400

    updated server file and username password

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,12 +7,24 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'scotthompson',
+      // Your MySQL password
+      password: 'root',
+      database: 'tracker'
+    },
+    console.log('Connected to the tracker database.')
+  );
 
-app.get('/', (req, res) => {
-    res.json({
-      message: 'Hello World'
-    });
-  });
+//app.get('/', (req, res) => {
+    //res.json({
+      //message: 'Hello World'
+   // });
+  //});
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
