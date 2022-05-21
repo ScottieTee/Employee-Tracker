6fa48d274e0b79e1c96bdf495177603af731aae6
Sarah Thompson <sthompson@fintechprocessing.com>
2022-05-21 10:55:12 -0400

    updating tables with foreign keys

--- db/schema.sql
+++ db/schema.sql
@@ -5,14 +5,10 @@ CREATE TABLE department (
 CREATE TABLE roles (
  id INTEGER AUTO_INCREMENT PRIMARY KEY,
  title VARCHAR(30) NOT NULL,
- salary DECIMAL,
- department_id INT );
+ salary DECIMAL);
 
 CREATE TABLE employees (
  id INTEGER AUTO_INCREMENT PRIMARY KEY,
  first_name VARCHAR(30) NOT NULL,
- last_name VARCHAR(30) NOT NULL,
- role_id INT,
- manager_id INT NULL);
+ last_name VARCHAR(30) NOT NULL);
 
- 
