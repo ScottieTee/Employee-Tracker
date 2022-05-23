057496092bf9f5c06f8357a349d5c7eca7e6edcb
Sarah Thompson <sthompson@fintechprocessing.com>
2022-05-23 10:29:14 -0400

    updated the roles and employees with foreign key constraints

--- db/schema.sql
+++ db/schema.sql
@@ -5,10 +5,14 @@ CREATE TABLE department (
 CREATE TABLE roles (
  id INTEGER AUTO_INCREMENT PRIMARY KEY,
  title VARCHAR(30) NOT NULL,
- salary DECIMAL);
+ salary DECIMAL,
+ department_id INTEGER,
+ CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL);
 
 CREATE TABLE employees (
  id INTEGER AUTO_INCREMENT PRIMARY KEY,
  first_name VARCHAR(30) NOT NULL,
- last_name VARCHAR(30) NOT NULL);
+ last_name VARCHAR(30) NOT NULL,
+ roles_id INTEGER,
+ CONSTRAINT fk_roles FOREIGN KEY (roles_id) REFERENCES roles(id) ON DELETE SET NULL);
 
